bb0d2561c4850e564fc1b67e8b8443e28dfc7838
Mu Qin <magenta2127@gmail.com>
2019-12-27 11:45:50 +0800

    invoke lifecycle

--- react/src/utils/invokeLifecycle.js
+++ react/src/utils/invokeLifecycle.js
@@ -4,3 +4,34 @@ export const invokeWillMountLifecycle = (c) => {
     c.componentWillMount();
   }
 }
+
+// invoke componentDidMount lifecycle
+export const invokeDidMountLifecycle = (c) => {
+  if (c.componentDidMount !== null) {
+    c._renderCallbacks.push(c.componentDidMount);
+  }
+}
+
+// invoke componentWillReceiveProps lifecycle
+export const invokeWillReceivePropsLifecycle = (c, newType, cctx) => {
+  const force = c._force;
+  if (newType.getDerivedStateFromProps === null && force === null && c.componentWillReceiveProps !== null) {
+    c.componentWillReceiveProps(newProps, cctx);
+  }
+}
+
+// invoke componentWillUpdate lifecycle
+export const invokeWillUpdateLifecycle = (c, newProps, cctx) => {
+  if (c.componentWillUpdate != null) {
+    c.componentWillUpdate(newProps, c._nextState, cctx);
+  }
+}
+
+// invoke componentDidUpdate lifecycle
+export const invokeDidUpdateLifecycle = (c, oldProps, oldState, snapshot) => {
+  if (c.componentDidUpdate != null) {
+    c._renderCallbacks.push(() => {
+      c.componentDidUpdate(oldProps, oldState, snapshot);
+    });
+  }
+}
