4891bc5c8040f4c10bd3e26f3fdb28397e9d79d7
Mu Qin <magenta2127@gmail.com>
2019-12-30 08:24:01 +0800

    add render method

--- react/src/component/componentClass.js
+++ react/src/component/componentClass.js
@@ -1,9 +1,64 @@
+import { assign } from '../utils/obj'
+import { enqueueRender } from '../render/renderQueue';
+import { Fragment } from '../create-element';
 /**
  * base compoent class.
  * @param {*} props initial component props
  * @param {*} context initial context from parent components
  */
-export const Component = (props, context) => {
+const Component = (props, context) => {
   this.props = props;
   this.context = context;
 }
+
+/**
+ * update component state and trigger re-render
+ * @param {object | ((s: object, p: object) => object)} update an object of
+ * state properties to update or a function that takes current state and props
+ * and return a new state,
+ * @param callback a function to be called once component state is updated
+ *
+ */
+Component.prototype.setState = function(update, callback) {
+  let s;
+  if (this._nextState !== this.state) {
+    s = this._nextState;
+  } else {
+    s = this._nextState = assign({}, this.state);
+  }
+
+  if (typeof update === 'function') {
+    update = update(s, this.props);
+  }
+
+  if (update) {
+    assign(s, update);
+  }
+
+  if (this._vnode) {
+    this._force = false;
+    if (callback) this._renderCallbacks.push(callback);
+    enqueueRender(this);
+  }
+}
+
+/**
+ * perform a synchronous re-render of the component
+ * @param callback a function to be called once component state is updated
+ */
+Component.prototype.forceUpdate = function(callback) {
+  if (this._vnode) {
+    // set render mode to force. forceUpdate should never call shouldComponentUpdate
+    this._force = true;
+    if (callback) this._renderCallbacks.push(callback);
+    enqueueRender(this);
+  }
+}
+
+/**
+ * accets `props` and `state`, and returns a new virtual dom tree.
+ *
+ */
+Component.prototype.render = Fragment;
+
+export default Component;
--- react/src/render/render.js
+++ react/src/render/render.js
@@ -2,6 +2,11 @@ import { commitRoot } from './commitQueue';
 import { diff } from '../diff';
 import { assign } from '../utils/obj';
 import { getDOMSibling, updateParentDomPointers } from './dom';
+import options from '../options';
+import { EMPTY_OBJ, EMPTY_ARR } from '../utils/constants'
+import { createElement, Fragment } from '../create-element';
+
+const IS_HYDRATE = EMPTY_OBJ;
 
 /** TODO.
  * trigger in-place re-rendering of a component.
@@ -27,4 +32,67 @@ export const renderComponent = (component) => {
   if (newDom !== oldDom) {
     updateParentDomPointers(vnode);
   }
+}
+
+/**
+ * render a virtual node into a DOM element. UPDATE SOURCE CODE.
+ * @param vnode virtual node to render
+ * @param parentDom the dom element to render into
+ * @param replaceNode attempt to re-use an existing DOM tree rooted at `replaceNode`
+ */
+export const render = (vnode, parentDom, replaceNode) => {
+  if (options._root) options._root(vnode, parentDom);
+
+  let isHydrating = replaceNode === IS_HYDRATE;
+  let oldVNode;
+  if (isHydrating) {
+    oldVNode = (replaceNode && replaceNode._children) || parentDom._children;
+    parentDom._children = vnode;
+  } else {
+    oldVNode = null;
+    if (replaceNode) {
+      replaceNode._children = vnode;
+    } else {
+      parentDom._children = vnode;
+    }
+  }
+
+  // TODO???
+  vnode = createElement(Fragment, null, [vnode]);
+
+  let commitQueue = [];
+  const isSvg = parentDom.ownerSVGElement !== undefined;
+
+  let excessDomChildren;
+  if (replaceNode && !isHydrating) {
+    excessDomChildren = [replaceNode]
+  } else {
+    excessDomChildren = oldVNode ? null : EMPTY_ARR.slice.call(parentDom.childNodes);
+  }
+
+  const oldDom = replaceNode || EMPTY_OBJ;
+
+  // UPDATE SOURCE CODE.
+  diff(
+    parentDom,
+    vnode,
+    oldVNode || EMPTY_OBJ,
+    EMPTY_OBJ,
+    isSvg,
+    excessDomChildren,
+    commitQueue,
+    oldDom,
+    isHydrating,
+  );
+
+  commitRoot(commitQueue, vnode);
+}
+
+/**
+ * update an existing DOM element with data from a virtual node
+ * @param vnode the virtual node to render
+ * @param parentDom the DOM element to update
+ */
+export const hydrate = (vnode, parentDom) => {
+  render(vnode, parentDom, IS_HYDRATE);
 }
