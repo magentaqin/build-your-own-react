8f9516b2c66f5903b7210a9c2e6ef98c510a2dcd
Mu Qin <magenta2127@gmail.com>
2020-01-02 20:57:24 +0800

    add workflow for rendering hello world

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,6 +1,14 @@
 import myReact, { x } from 'my-react';
 
 class App extends myReact.Component {
+  componentWillMount() {
+    console.log('I WILL MOUNT.');
+  }
+
+  componentDidMount() {
+    console.log('I AM MOUNTED.')
+  }
+
   render() {
     return (
       <h1>Hello World</h1>
